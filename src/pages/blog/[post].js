32bd70b39c6a3256954c8b88e5a1131e39b24e7b
Stanley Zhao <stanleyhzhao@gmail.com>
2022-12-06 13:40:39 -0500

    Update [post].js

--- src/pages/blog/[post].js
+++ src/pages/blog/[post].js
@@ -96,6 +96,24 @@ export default function Post({ postContent }) {
             </ReactMarkdown>
           </article>
         </div>
+        {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-10 sm:w-12 h-auto hover:stroke-[#3b82f6] transition duration-300 cursor-pointer"
+          onClick={() => {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+          }}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg> */}
       </div>
     </>
   );
