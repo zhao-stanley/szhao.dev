91810cea684759715847bfec5033f8d4e5197b34
Stanley Zhao <stanleyhzhao@gmail.com>
2022-12-06 14:29:37 -0500

    Change order of icons

--- src/components/home/FavoriteProjects.js
+++ src/components/home/FavoriteProjects.js
@@ -51,22 +51,6 @@ export default function FavoriteProjects({ children }) {
                       </h3>
                     </div>
                     <section className="flex flex-row gap-x-4 items-center">
-                      <Link href={p.link} target="_blank" rel="opener">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={2}
-                          stroke="currentColor"
-                          className="w-6 h-6 stroke-white hover:brightness-[80%] transition duration-300"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                          />
-                        </svg>
-                      </Link>
                       {p.github ? (
                         <Link
                           href={`https://github.com/${p.github}`}
@@ -83,6 +67,22 @@ export default function FavoriteProjects({ children }) {
                           </svg>
                         </Link>
                       ) : null}
+                      <Link href={p.link} target="_blank" rel="opener">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={2}
+                          stroke="currentColor"
+                          className="w-6 h-6 stroke-white hover:brightness-[80%] transition duration-300"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                          />
+                        </svg>
+                      </Link>
                     </section>
                   </div>
                   <hr
