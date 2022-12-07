aa4629c31804cf554a927fa328281ba95838d6d4
Stanley Zhao <stanleyhzhao@gmail.com>
2022-12-07 08:00:06 -0500

    Update siteMetadata.js

--- src/data/siteMetadata.js
+++ src/data/siteMetadata.js
@@ -1,6 +1,6 @@
 const siteMetadata = {
-  defaultTheme: "light",
-  themeColor: "#E77E7E",
+  defaultTheme: "dark",
+  themeColor: "#3b82f6",
   title: "Stanley Zhao - Software Engineer, Student, Cybersecurity Enthusiast",
   desc: "I develop software, study cybersecurity, and occassionally write :)",
   tags: "stanley, zhao, scilynk, discussmed",
