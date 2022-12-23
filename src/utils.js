b1faa8451361adba9ef590e835cbc92ca9ab7045
Stanley Zhao <stanleyhzhao@gmail.com>
2022-12-23 09:01:11 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,10 @@ export default function properCase(text) {
     rest = text.replace("js", "JS");
   } else if (text.includes("css")) {
     rest = text.replace("css", "CSS");
+  } else if (text.includes("html")) {
+    rest = text.replace("html", "HTML");
+  } else if (text.includes("javascript")) {
+    rest = text.replace("javascript", "JavaScript");
   } else {
     rest = text.replace("db", "DB");
   }
