afa2f96c4d2db8dcf06d496b22f4dd105fe08435
Stanley Zhao <stanleyhzhao@gmail.com>
2023-03-01 09:23:45 -0500

    Update siteMetadata.js

--- src/data/siteMetadata.js
+++ src/data/siteMetadata.js
@@ -1,5 +1,5 @@
 const siteMetadata = {
-  defaultTheme: "dark",
+  defaultTheme: "light",
   themeColor: "#3b82f6",
   title: "Stanley Zhao - Software Engineer, Student, Cybersecurity Enthusiast",
   desc: "Hey! I'll be studying computer science at MIT this fall. My academic interests include web development, competitive programming, and cybersecurity.",
