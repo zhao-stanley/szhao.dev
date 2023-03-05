0219b61e3ae08ed9acdc7dfc75265e98184afcf0
zhao-stanley <stanleyhzhao@gmail.com>
2023-03-05 00:41:33 -0500

    Logo

--- src/components/global/SEO.js
+++ src/components/global/SEO.js
@@ -35,7 +35,7 @@ export default function SEO({ title, desc }) {
       />
       <meta
         property="og:image"
-        content="https://www.szhao.dev/static/img/profile.png"
+        content="https://www.szhao.dev/static/img/logo.png"
       />
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://www.szhao.dev/" />
@@ -49,7 +49,7 @@ export default function SEO({ title, desc }) {
       />
       <meta
         property="twitter:image"
-        content="https://www.szhao.dev/static/img/profile.png"
+        content="https://www.szhao.dev/static/img/logo.png"
       />
     </Head>
   );
