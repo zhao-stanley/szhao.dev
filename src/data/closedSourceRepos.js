f342e1d34325328e1c7ebcdb7b79cea3e77ec39d
zhao-stanley <stanleyhzhao@gmail.com>
2023-03-08 18:19:55 -0500

    Update closedSourceRepos.js

--- src/data/closedSourceRepos.js
+++ src/data/closedSourceRepos.js
@@ -11,18 +11,6 @@ const closedSourceRepos = [
     desc: "Publisher of the most popular, memorable, and cutting-edge browser games, completely free.",
     lang: "JavaScript",
   },
-  {
-    name: "UBG100",
-    link: "https://ubg100.github.io/",
-    desc: "Predecessor to WebLFG, publisher of the most popular, memorable, and cutting-edge browser games, completely free.",
-    lang: "HTML",
-  },
-  {
-    name: "joseph.cf",
-    link: "https://joseph.cf/",
-    desc: "Portfolio for a musical artist created with React and styled with TailwindCSS.",
-    lang: "JavaScript",
-  },
 ];
 
 export default closedSourceRepos;
